cb6ff118099a02386cc1eb4bc68c6295c994e7bb
Vova <dvolodya@ukr.net>
2022-12-30 15:30:32 +0200

    add Mapbox sdk

--- SaparTaxi/ViewController.swift
+++ SaparTaxi/ViewController.swift
@@ -6,12 +6,20 @@
 //
 
 import UIKit
+import MapboxMaps
 
 class ViewController: UIViewController {
 
+    private var mapView: MapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let myResourceOptions = ResourceOptions(accessToken: "pk.eyJ1IjoibWVpcmxlbiIsImEiOiJjbDhmejY3Y3cwMXQzM3NwNGFuN2hwaW4yIn0.e6jsQjtaKQGtzM5ijU4oJQ")
+        let myMapInitOptions = MapInitOptions(resourceOptions: myResourceOptions)
+        mapView = MapView(frame: view.bounds, mapInitOptions: myMapInitOptions)
+        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(mapView)
     }
 
 
