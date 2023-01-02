40fc6ab0ba7abd572b3cafeaa28d75444437063f
Maxim-Deriglazov <110385001+Maxim-Deriglazov@users.noreply.github.com>
2023-01-02 11:52:41 +0200

    Up screen Payment
    Show addCompanion, plus, minus

--- SaparTaxi/UI/PaymentViewController.swift
+++ SaparTaxi/UI/PaymentViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class PaymentViewController: UIViewController {
     
+    @IBOutlet weak var titleSwichLabel: UILabel?
+    @IBOutlet weak var companionSwich: UISwitch?
+    @IBOutlet weak var addCompanionView: UIView?
+    @IBOutlet weak var amountLabel: UILabel?
+    @IBOutlet weak var minusButton: UIButton?
+    @IBOutlet weak var plusButton: UIButton?
+    
+    var amountCompanion = 1
+    
     override func viewDidLoad() {
         self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
         self.navigationController?.navigationBar.shadowImage = UIImage()
@@ -20,4 +29,29 @@ class PaymentViewController: UIViewController {
         
         navigationItem.leftBarButtonItem = createBackButton()
     }
+    
+    @IBAction func companionIsActiv(_ sender: UISwitch) {
+        guard let companionSwichIsOn = companionSwich?.isOn else { return }
+        addCompanionView?.isHidden = !companionSwichIsOn
+        titleSwichLabel?.text = companionSwichIsOn == false ? "Включить режим попутчика" : "Режим попутчика активен"
+    }
+    
+    @IBAction func amountReduce(_ sender: UIButton) {
+        if amountCompanion <= 2 {
+            minusButton?.isEnabled = false
+        }
+        plusButton?.isEnabled = true
+        amountCompanion -= 1
+        amountLabel?.text = String(amountCompanion)
+    }
+    
+    @IBAction func amountRaise(_ sender: UIButton) {
+        if amountCompanion >= 3{
+            plusButton?.isEnabled = false
+        }
+        minusButton?.isEnabled = true
+        amountCompanion += 1
+        amountLabel?.text = String(amountCompanion)
+    }
+    
 }
